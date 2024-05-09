a2a8fea4acea5e5310c5d861ddcbaa2572ddfbf4
Hanfeng23333 <3065613494@qq.com>
2024-05-09 18:25:57 +0800

    update

--- Libs/Bot_lib.py
+++ Libs/Bot_lib.py
@@ -1,5 +1,5 @@
 #Made by Han_feng
-import httpx,asyncio
+import httpx,asyncio,functools
 from Plugins.Plugin_template import Base_plugin
 from Tool_lib import *
 
@@ -11,7 +11,7 @@ class QQbot:
         self.master_qq = master_qq
         self.url = url
         self.session_key = ""
-        self.whitegroups:list[int] = []
+        self.white_groups:list[int] = []
 
         #Plugins management
         self.plugins:list[Base_plugin] = []
@@ -22,6 +22,7 @@ class QQbot:
 
         #Message management
         self.send_message_queue:list[Message] = []
+        self.send_message_tasks = set()
         self.massage_client = httpx.AsyncClient()
 
     async def login(self) -> None:
@@ -45,7 +46,7 @@ class QQbot:
             #Closure function
             def push_message(message:Message):
                 if isinstance(message,Message):
-                    message.push("\n—— by %s Plugin"%plugin.name)
+                    message.push("\n—— by %s Plugin"%plugin)
                     self.send_message_queue.append(message)
             
             plugin.push_message = push_message
@@ -53,21 +54,22 @@ class QQbot:
             plugin.async_client = self.plugin_client
 
             self.plugins.append(plugin)
-            print("%s plugin is loaded successfully!"%plugin.name)
+            print("%s plugin is loaded successfully!"%plugin)
         else:
-            print("%s plugin is loaded failed!"%plugin.name)
+            print("%s plugin is loaded failed!"%plugin)
 
-    def remove_plugin(self,plugin:Base_plugin,message:str="",exception:BaseException=None) -> None:
-        message_text = Message_maker.message_text(message)
-        exception_text = Message_maker.message_text("%s: %s\n"%(type(exception).__name__,str(exception)) if exception else "")
-        remove_text = Message_maker.message_text("%s Plugin has been removed!",plugin.name)
-        for target in self.whitegroups:
-            error_message = Message(target)
-            error_message.push(message_text)
-            error_message.push(exception_text)
-            error_message.push(remove_text)
-            self.send_message_queue.append(error_message)
-        self.plugins.remove(plugin)
+    def remove_plugin(self,plugin:Base_plugin|str,message:str="",exception:Exception=None) -> None:
+        if plugin in self.plugins:
+            message_text = Message_maker.message_text(message)
+            exception_text = Message_maker.message_text("%s: %s\n"%(type(exception).__name__,exception) if exception else "")
+            remove_text = Message_maker.message_text("%s Plugin has been removed!"%plugin)
+            for target in self.white_groups:
+                error_message = Message(target)
+                error_message.push(message_text)
+                error_message.push(exception_text)
+                error_message.push(remove_text)
+                self.send_message_queue.append(error_message)
+            self.plugins.remove(plugin)
 
     def get_plugin(self,plugin_name:str) -> Base_plugin | None:
         #Callback function for plugins
@@ -75,23 +77,66 @@ class QQbot:
             return self.plugins[self.plugins.index(plugin_name)]
         except ValueError:
             return None
+        
+    def check_error(self,plugin_collection:set,task:asyncio.Task):
+        task_exception = task.exception()
+        if task_exception:
+            self.remove_plugin(task.get_name(),exception=task_exception)
+        plugin_collection.discard(task)
 
     async def fetch_events(self):
         received_events_result = await self.massage_client.get(self.url+"/fetchMessage",params={"sessionKey":self.session_key,"count":20})
-        self.receive_event_queue.extend(received_events_result.json()["data"])
+        for event in received_events_result.json()["data"]:
+            event_dict:dict = {"event_type":"","key_word":"","args":[],"info":{"sender":0,"group":0}}
+            is_valid:bool = False
+            match event["type"]:
+                case "GroupMessage" if event["sender"]["group"] in self.white_groups:
+                    #Group message
+                    event_dict["event_type"] = "command"
+                    event_dict["info"]["sender"] = event["sender"]["id"]
+                    event_dict["info"]["group"] = event["sender"]["group"]
+
+                    #search the command
+                    command_symbol_count = 0 #handle the exception
+                    for message in event["messageChain"]:
+                        match message["type"]:
+                            case "At" if message["target"] == self.qq:
+                                is_valid = True
+                            case "Plain":
+                                text_segments = list(filter(None,message["text"]))
+                                if text_segments and text_segments[0][0] == "/":
+                                    command_symbol_count += 1
+                                    event_dict["key_word"] = text_segments.pop(0)[1:]
+                                event_dict["args"].extend(text_segments)
+                    
+                    #Let's see whether the command is vdlid
+                    is_valid = is_valid or command_symbol_count and command_symbol_count <= 1
+                
+                case "NudgeEvent" if event["target"] == self.qq:
+                    event_dict["info"]["sender"] = event["fromID"]
+                    is_valid = True
+                    if event["subject"]["kind"] == "Group":
+                        event_dict["info"]["group"] = event["subject"]["id"]
+                        is_valid = event["subject"]["id"] in self.white_groups
+
 
     async def update_plugins(self):
         while True:
-            update_tasks:dict[Base_plugin,asyncio.Task] = dict(map(lambda plugin:(plugin,asyncio.create_task(asyncio.to_thread(plugin.update))),self.plugins))
+            #Create the task set
+            update_tasks:set[asyncio.Task] = set()
+            for plugin in self.plugins:
+                task = asyncio.create_task(asyncio.to_thread(plugin.update))
+                task.set_name(plugin.name)
+                task.add_done_callback(functools.partial(self.check_error,update_tasks)) #automatically garbage collection
+                update_tasks.add(task)
+            
+            #Wait for half an hour
             await asyncio.sleep(1800)
 
-            for plugin in update_tasks:
-                if update_tasks[plugin].done():
-                    exception = update_tasks[plugin].exception()
-                    if exception:
-                        self.remove_plugin(plugin,exception)
-                else:
-                    self.remove_plugin(plugin,TimeoutError("update timed out!"))
+            #Check the tasks that haven't finished, which must be something wrong...
+            for task in update_tasks:
+                plugin_name = task.get_name()
+                self.remove_plugin(plugin_name,exception=TimeoutError("The update of the %s plugin timed out!"%plugin_name))
     
     async def main(self):
         pass
--- Plugins/Plugin_template.py
+++ Plugins/Plugin_template.py
@@ -6,15 +6,16 @@ import asyncio,httpx
 
 class Base_plugin:
     def __init__(self):
-        self.key_words = [] #Fill the list of the key words, as only the matched functions will call the reply function
         self.name = type(self).__name__
-        self.data_path = "Plugins/Plugins_data/"+self.name+"/" #You must use this data path to store your data or file, or they won't save correctly
+        self.data_path = "Plugins/Plugins_data/%s/"%self.name #You must use this data path to store your data or file, or they won't save correctly
         self.plugin_lock = False
+        self.event_types = [] #choose which kinds of event to receive
+        self.key_words = [] #Fill the list of the key words, as only the matched functions will call the reply function when the event type is "command"
         self.push_message:function[Message] = None
         self.get_plugin:function[str] = None
         self.async_client:httpx.AsyncClient = None
 
-    async def reply(self,key_word:str,*args,**info) -> None:
+    async def reply(self,event_type:str,key_word:str="",*args,**info) -> None:
         """
         Overwrite this reply function for calling your functions to reply the users.
 
@@ -26,11 +27,13 @@ class Base_plugin:
 
         Params:
 
+        event_type(str) -> the type of the event
+
         key_word(str) -> the key word that calls this plugin
 
         args(list) -> the parameters that follow the key word
 
-        info(dict) -> the information of the message
+        info(dict) -> the information of the message {"sender":qq(int),"group":qq(int)}
         """
 
     def update(self) -> None:
@@ -45,13 +48,27 @@ class Base_plugin:
 
         Use the get_plugin function to interact with other plugins
 
+        Set the plugin_lock to prevent some strange error if necessary
+
         DON'T MODIFY THE PARAMS, OR YOUR PLUGIN WILL BE UNABLE TO USE!!!
         """
 
-    def __hash__(self):
+    def help(self) -> dict:
+        """
+        Return the help documentation for your plugin
+
+        Format(keys must be str and values can either be str or nested dict(keys must be str,too)):
+        {function_name:introduction,function_name:{arg1:introduction,arg2:introduction...}...}
+        """
+        return {}
+
+    def __str__(self) -> str:
+        return self.name
+
+    def __hash__(self) -> int:
         return hash(self.name)
     
-    def __eq__(self,other):
+    def __eq__(self,other) -> bool:
         if isinstance(other,str):
             return self.name == other
         elif isinstance(other,Base_plugin):
